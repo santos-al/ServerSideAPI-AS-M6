61d4156d28561c2bfe3d88f2976afd345b142a32
Xandi <santosal2018@gmail.com>
2023-08-05 19:43:41 -0400

    move folders and add cvity search function

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,4 +1,30 @@
-const apiKey = '7eed7bdfa63d6867ee3f86d85b1a99a7';
+const searchInput = document.querySelector('#search-input');
+const search = document.querySelector('#search-form')
 
-const apiURL = ''
 
+const apiKey = '97e80e809f60ac68aefc5b2dbb2deb2f';
+let city;
+
+
+
+function userSearch(e) {
+    e.preventDefault();
+    let citySearch = searchInput.value;
+    console.log(citySearch);
+    findCityWeather(citySearch);
+
+}
+
+function findCityWeather(cityName) {
+    let apiCall = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey};`
+
+    fetch(apiCall)
+    .then(function (res) {
+      console.log(res.json());
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
+search.addEventListener('submit', userSearch);
