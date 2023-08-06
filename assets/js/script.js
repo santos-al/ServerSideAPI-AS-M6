a9220cdd58425065e5d190dc04fc247cd0018585
Xandi <santosal2018@gmail.com>
2023-08-06 14:13:38 -0400

    add variables and html references

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,8 @@
-const searchInput = document.querySelector('#search-input');
-const search = document.querySelector('#search-form')
-
-
-const apiKey = '97e80e809f60ac68aefc5b2dbb2deb2f';
-let city;
-
-
-
-function userSearch(e) {
-    e.preventDefault();
-    let citySearch = searchInput.value;
-    console.log(citySearch);
-    findCityWeather(citySearch);
-
-}
-
-function findCityWeather(cityName) {
-    let apiCall = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey};`
-
-    fetch(apiCall)
-    .then(function (res) {
-      console.log(res.json());
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
-}
-
-search.addEventListener('submit', userSearch);
+var searchHistory = [];
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = '97e80e809f60ac68aefc5b2dbb2deb2f';
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
